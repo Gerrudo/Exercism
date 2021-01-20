358adbc9126a53ed29c35bba522d096b175554d0
Gerrudo <tom@tomsnetwork.uk>
2021-01-20 12:24:44 +0000

    Updated solution

--- javascript/triangle/triangle.js
+++ javascript/triangle/triangle.js
@@ -7,11 +7,10 @@ export class Triangle {
   constructor(...sides) {
     this.sides = sides;
   }
-  
+
   get isEquilateral() {
-    if (this.sides[0,1,2] === 0) {
-      return false;
-    }else if (this.sides[0,1,2] && this.sides[1,2,0] === this.sides[2,0,1]) {
+    let newSides = this.findType(this.sides);
+    if (newSides.length === 0) {
       return true;
     }else {
       return false;
@@ -19,21 +18,45 @@ export class Triangle {
   }
 
   get isIsosceles() {
-    if (this.sides[0,1,2] === 0){
-      return false
-    }else if(this.sides[0] === this.sides[1,2] || this.sides[1] === this.sides[0,2] || this.sides[2] === this.sides[0,1]) {
+    let newSides = this.findType(this.sides);
+    if (newSides.length === 1) {
       return true;
     }else {
-      return false
+      return false;
     }
   }
 
   get isScalene() {
-
+    let newSides = this.findType(this.sides);
+    if (newSides.length === 3) {
+      return true;
+    }else {
+      return false;
+    }
   }
+
+  findType(sides) {
+    let i;
+    for (i = 0; i < sides.length; i++) {
+      if (sides[i] === sides[0,1,2]) {
+        sides.splice(sides.indexOf(i));
+      }
+    }
+    return sides;
+  }
+
 }
 
 /*
 We could possibily look at running a for loop in the array, and once the action is finished, if a tringle side doesn't meet equirments,
 Then it is spliced from the array, leaving only 2 or less elements to compare. 
+
+for each in sides 
+  if i equal to sides[0,1,2]
+    splice.sides[i]
+  
+if sides.length === 1 //for equalateral value should be 0, for isoceles 1 and for scalene 3
+  return true
+else
+  return false
 */
