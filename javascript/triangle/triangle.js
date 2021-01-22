1a8fb4b74c226502275ab6937368ea22d83b2cb0
Gerrudo <tom@tomsnetwork.uk>
2021-01-22 18:15:18 +0000

    Solved. Don't wanna see this code ever again

--- javascript/triangle/triangle.js
+++ javascript/triangle/triangle.js
@@ -9,55 +9,36 @@ export class Triangle {
   }
 
   get isEquilateral() {
-    let newSides = this.findType(this.sides);
-    if (newSides.length === 0) {
+    let [a, b, c] = [...this.sides];
+    if (this.sides.reduce(function(a, b){return a + b;}, 0) === 0) {
+      return false;
+    }else if (a === b && b === c && c === a) {
       return true;
-    }else {
-      return newSides.length;
     }
+    return false;
   }
 
   get isIsosceles() {
-    let newSides = this.findType(this.sides);
-    if (newSides.length === 1) {
-      return true;
-    }else {
+    let [a, b, c] = [...this.sides];
+    if (this.sides.reduce(function(a, b){return a + b;}, 0) === 0) {
       return false;
+    }else if (a+b<c || b+c<a || c+a<b) {
+      return false;
+    }else if (a === b || b === c || c === a) {
+      return true;
     }
+    return false;
   }
 
   get isScalene() {
-    let newSides = this.findType(this.sides);
-    if (newSides.length === 3) {
-      return true;
-    }else {
+    let [a, b, c] = [...this.sides];
+    if (this.sides.reduce(function(a, b){return a + b;}, 0) === 0) {
       return false;
+    }else if (a+b<c || b+c<a || c+a<b) {
+      return false;
+    }else if (a !== b && b !== c && c !== a) {
+      return true;
     }
+    return false;
   }
-
-  findType(sides) {
-    let i;
-    for (i = 0; i < 3; i++) {
-      //issues lie here, as we can't statically check the elements as it will always return true
-      if (sides[i] === sides[0] || sides[i] === sides[1] || sides[i] === sides[2]) {
-        sides.splice(sides.indexOf(i), 1);
-      }
-    }
-    return sides;
-  }
-
 }
-
-/*
-We could possibily look at running a for loop in the array, and once the action is finished, if a tringle side doesn't meet equirments,
-Then it is spliced from the array, leaving only 2 or less elements to compare. 
-
-for each in sides 
-  if i equal to sides[0,1,2]
-    splice.sides[i]
-  
-if sides.length === 1 //for equalateral value should be 0, for isoceles 1 and for scalene 3
-  return true
-else
-  return false
-*/
