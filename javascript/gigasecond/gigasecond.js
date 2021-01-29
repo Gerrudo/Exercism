eae04001e9f89485f011e7129d3dd44ccb40173a
Gerrudo <tom@tomsnetwork.uk>
2021-01-29 21:17:51 +0000

    I'm code gamer and i am bad at coderin programerin

--- javascript/gigasecond/gigasecond.js
+++ javascript/gigasecond/gigasecond.js
@@ -6,5 +6,6 @@
 const oneGigasecond = 10**9;
 
 export const gigasecond = (inputDate) => {
-  return new Date(new Date(inputDate.getTime()).setUTCSeconds(inputDate.getUTCSeconds()+oneGigasecond));
+  //This is multiplied by 1000 as JS Epoch Time is in milliseconds.
+  return new Date(inputDate.getTime()+oneGigasecond * 1000);
 };
