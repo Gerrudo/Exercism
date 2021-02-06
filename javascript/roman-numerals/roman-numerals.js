3d79857e267984077e4ab9f2f1ce31f860a8dda9
Gerrudo <tom@tomsnetwork.uk>
2021-02-06 14:14:58 +0000

    Solution for Roman Numerals

--- javascript/roman-numerals/roman-numerals.js
+++ javascript/roman-numerals/roman-numerals.js
@@ -3,23 +3,19 @@
 // convenience to get you started writing code faster.
 //
 
-export const toRoman = (...int) => {
-  const map = { M:1000,CM:900,D:500,CD:400,C:100,XC:90,L:50,XL:40,X:10,IX:9,V:5,IV:4,I:1 };
+export const toRoman = (int) => {
+  const rnMap = { M:1000,CM:900,D:500,CD:400,C:100,XC:90,L:50,XL:40,X:10,IX:9,V:5,IV:4,I:1 };
+  let romanNum= '';
   /*
-  let roman ='';
-  for(let i in map){
-    
-    while(int >= map[i]){
-      roman += i;
-      int -= map[i];
+    this foreach/while will compare the int to each number in rmMap, once the number it has reached in rmMap is greater than, 
+    or equal to int, it will add the assigned letter to romanNum. Finally, it will subtrack the added value from int, 
+    and start the loop again. 
+  */
+  for(let i in rnMap){
+    while(int >= rnMap[i]){
+      romanNum += i;
+      int -= rnMap[i];
     }
   }
-*/
-  const roman = int.map(x => (
-      map[x],
-      int.splice(map[x])
-    )
-  );
-  
-  return roman;
+  return romanNum;
 };
