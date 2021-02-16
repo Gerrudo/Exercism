06ab376233fa992700baee4d387ce601ec733c21
Gerrudo <tom@tomsnetwork.uk>
2021-02-16 12:44:33 +0000

    Updated clock task

--- javascript/clock/clock.js
+++ javascript/clock/clock.js
@@ -3,19 +3,30 @@
 // convenience to get you started writing code faster.
 //
 
+import { time } from "console";
+
 export class Clock {
   constructor(hour = 0, minutes = 0) {
     this.hour = hour;
     this.minutes = minutes;
+    this.timeToRtn;
+    this.stringify = (time) => {return time.toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})};
   }
 
   toString() {
-    const stringify = toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false});
-    
-    if (this.hour < 24 && this.minutes < 60) {
-      return `${this.hour.stringify}:${this.minutes.stringify}`
-    }else {
-      return `${Math.floor(this.hour / 24)}:${Math.floor(this.minutes / 60)}`
+    if (this.hour < 23 && this.minutes < 59) {
+      return `${this.stringify(this.hour)}:${this.stringify(this.minutes)}`;
+    }else{
+      this.hour = Math.floor(this.hour / 25);
+      this.minutes = Math.floor(this.minutes / 61);
+      if (this.hour <= 24){
+        this.hour - 24
+      }
+      if (this.minutes <= 60){
+        this.hour + 1
+        this.minutes - 60
+      }
+      return `${this.stringify(this.hour)}:${this.stringify(this.minutes)}`;
     }
   }
 
