226ac1232674a52df690cb023a69843c7888fce3
Gerrudo <tom@tomsnetwork.uk>
2021-03-03 17:43:46 +0000

    Moved and Renamed 2 consts in Space Age

--- javascript/space-age/space-age.js
+++ javascript/space-age/space-age.js
@@ -3,17 +3,18 @@
 // convenience to get you started writing code faster.
 //
 
+const ORBITAL_PERIODS = {
+  'mercury': 0.2408467,
+  'venus': 0.61519726,
+  'earth': 1.0,
+  'mars': 1.8808158,
+  'jupiter': 11.862615,
+  'saturn': 29.447498,
+  'uranus': 84.016846,
+  'neptune': 164.79132
+};
+const EARTH_YEAR_IN_SECONDS = 31557600;
+
 export const age = (planet, seconds) => {
-  const orbPeriods = {
-    'mercury': 0.2408467,
-    'venus': 0.61519726,
-    'earth': 1.0,
-    'mars': 1.8808158,
-    'jupiter': 11.862615,
-    'saturn': 29.447498,
-    'uranus': 84.016846,
-    'neptune': 164.79132
-  };
-  const earthYear = 31557600;
-  return Math.round((seconds / earthYear) / orbPeriods[planet] * 100) / 100;
+  return Math.round((seconds / EARTH_YEAR_IN_SECONDS) / ORBITAL_PERIODS[planet] * 100) / 100;
 };
