19f0b51af5f06beaadda9c1a8f39aefd642f4739
Gerrudo <tom@tomsnetwork.uk>
2021-03-13 13:10:54 +0000

    Now splcing from correct positions, last test fail

--- javascript/pangram/pangram.js
+++ javascript/pangram/pangram.js
@@ -9,17 +9,17 @@ export const isPangram = ([...sentence]) => {
   let alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
   sentence.map(x => {
-    if(alphabet.indexOf(x) >= 0) {
-      alphabet.splice(sentence.indexOf(x), 1)
+    if(alphabet.indexOf(x, 0) >= 0) {
+      alphabet.splice(alphabet.indexOf(x), 1)
     }
   })
 
   if (alphabet.length <= 26){
-    return alphabet.length
-    //return true;
+    //return alphabet.length
+    return true;
   }
   else {
-    return alphabet.length;
-    //return false;
+    //return alphabet.length;
+    return false;
   }
 };
