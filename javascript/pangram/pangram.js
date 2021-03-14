196459847957a3fd4a3db52d5cd657f17dc4804a
Gerrudo <tom@tomsnetwork.uk>
2021-03-14 15:49:21 +0000

    general syntax change

--- javascript/pangram/pangram.js
+++ javascript/pangram/pangram.js
@@ -3,7 +3,6 @@
 // convenience to get you started writing code faster.
 //
 
-
 export const isPangram = ([...sentence]) => {
 
   let alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
@@ -14,7 +13,7 @@ export const isPangram = ([...sentence]) => {
     }
   })
 
-  if (alphabet.length == 0){
+  if (alphabet.length === 0){
     return true;
   }else {
     return false;
