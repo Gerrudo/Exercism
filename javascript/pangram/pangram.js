aead64f7858d8aec84e04cbd85261e111a9d06a2
Gerrudo <tom@tomsnetwork.uk>
2021-05-05 22:13:09 +0100

    Revised using every and includes, fails on test 9

--- javascript/pangram/pangram.js
+++ javascript/pangram/pangram.js
@@ -3,19 +3,8 @@
 // convenience to get you started writing code faster.
 //
 
-export const isPangram = ([...sentence]) => {
-
-  let alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-
-  sentence.map(x => {
-    if(alphabet.indexOf(x.toLowerCase(), 0) >= 0) {
-      alphabet.splice(alphabet.indexOf(x), 1)
-    }
-  })
-
-  if (alphabet.length === 0){
-    return true;
-  }else {
-    return false;
-  }
+export const isPangram = (sentence) => {
+  const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+  const test = (letter) => sentence.includes(letter);
+  return alphabet.every(test)
 };
