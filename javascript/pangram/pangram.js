85b478eae27b307a54cb2a02e5953fb5c7b8deb9
Gerrudo <tom@tomsnetwork.uk>
2021-05-06 11:16:58 +0100

    Fully working, formatted input using regex

--- javascript/pangram/pangram.js
+++ javascript/pangram/pangram.js
@@ -3,8 +3,9 @@
 // convenience to get you started writing code faster.
 //
 
+const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
 export const isPangram = (sentence) => {
-  const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-  const test = (letter) => sentence.includes(letter);
-  return alphabet.every(test)
+  const checkSentence = (letter) => sentence.replace("[^a-zA-Z]", "").toLowerCase().includes(letter);
+  return alphabet.every(checkSentence);
 };
